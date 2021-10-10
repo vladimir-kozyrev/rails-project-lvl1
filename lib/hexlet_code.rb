29763133c8d593c637206af8d0105acddf6ada74
Vladimir Kozyrev <v.kozyrev.sa@gmail.com>
2021-10-10 18:06:07 +0000

    replace require_relative with autoload

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 
-# I cannot autoload :Inputs
-# LoadError: cannot load such file -- hexlet_code/inputs
-require_relative 'hexlet_code/inputs/base'
-require_relative 'hexlet_code/inputs/string_input'
-require_relative 'hexlet_code/inputs/text_area'
-require_relative 'hexlet_code/inputs/select'
-
 # The root module
 module HexletCode
-  autoload :VERSION, 'hexlet_code/version'
-  autoload :Tag, 'hexlet_code/tag'
-  autoload :InputRenderer, 'hexlet_code/input_renderer'
-  autoload :Form, 'hexlet_code/form'
+  autoload :VERSION, 'hexlet_code/version.rb'
+  autoload :Tag, 'hexlet_code/tag.rb'
+  autoload :InputRenderer, 'hexlet_code/input_renderer.rb'
+  autoload :Form, 'hexlet_code/form.rb'
+  autoload :Inputs, 'hexlet_code/inputs.rb'
 
   def self.form_for(user, url: nil)
     form = Form.new(user, url)
---
+++ lib/hexlet_code/inputs.rb
@@ -0,0 +1,8 @@
+module HexletCode
+  module Inputs
+    autoload :Base, 'hexlet_code/inputs/base.rb'
+    autoload :Select, 'hexlet_code/inputs/select.rb'
+    autoload :StringInput, 'hexlet_code/inputs/string_input.rb'
+    autoload :TextArea, 'hexlet_code/inputs/text_area.rb'
+  end
+end
